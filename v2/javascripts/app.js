748a57cd7c05fbf96665dddad1c59e81af8209f8
jonavano <jonathanvanoudheusden@gmail.com>
2018-12-05 21:44:26 +0100

    added win and los

--- v2/javascripts/app.js
+++ v2/javascripts/app.js
@@ -3,6 +3,9 @@ var smolship = [55, 55];
 var largeship = [ 1, 23, 45 , 65];
 var ships = [smolship, largeship];
 
+var clicks = 0;
+var hits = 0;
+
 
 var main = function () {
     "use strict";
@@ -24,6 +27,8 @@ var main = function () {
 
     // });
 
+    
+
     $(".leftField div").toArray().forEach(function (element) {
         // create a click handler for this element
         $(element).on("click", function () {
@@ -37,6 +42,7 @@ var main = function () {
                 if(ships[index].includes(id)){
                         console.log("that's a hit");
                         $element.addClass("hit");
+                        hits++;
                 }
         }
 
@@ -52,7 +58,45 @@ var main = function () {
 
            // this.disable = true;
             // element.addClass("active");
-           
+
+            clicks ++;
+
+            if(hits == 14){
+                    alert("winner, winner chicken dinner!");
+            }
+            if(clicks == 49){
+                    alert("you lost fam");
+            }
+            console.log(clicks);
+
+        //     var count = 0;
+        //     for(var int = 1; int <= 49; int ++){
+        //         var test = $(".leftField div:nth-child(" +int+")");
+        //         console.log("problem" +  test.hasClass("clicked") + " " + int);
+
+        //         if(test.hasClass("clicked") != true){
+        //                 // test.includes("clicked");
+        //                 count ++;
+                       
+        //                 console.log("this si the problem" +  test.hasClass("clicked") );
+        //                 return false;
+        //         }
+        //         if(count == 49){
+        //                 alert("you lost");
+        //         }
+        //         console.log("it works" + count);
+                
+        //         // if($(".leftField div:nth-child(" +int+")")){
+                        
+        //         //         console.log("this workds");
+        //         // }
+        //         // console.log($(".leftField div:nth-child(" + int +")"));
+        //         // if(document.leftField.getElementById(int).clicked != "clicked"){
+        //         //         return false;
+        //         // }
+        //         // alert("wiw");
+        // };
+        // alert("you lost");
             return false;
         });
         });
@@ -85,13 +129,25 @@ $(".rightField div").toArray().forEach(function (element) {
 
 
         // for(var int = 1; int <= 49; int ++){
-        //         if($(".leftField div:nth-child(" + int +")").
+        //         var test = $(".leftField div:nth-child(" +int+")");
+
+        //         if(test.hasClass("clicked") != true){
+        //                 // return false;
+
+        //                 console.log("this si the problem");
+        //         }
+        //         console.log(test.hasClass("clicked"));
+                
+        //         // if($(".leftField div:nth-child(" +int+")")){
+                        
+        //         //         console.log("this workds");
+        //         // }
         //         // console.log($(".leftField div:nth-child(" + int +")"));
         //         // if(document.leftField.getElementById(int).clicked != "clicked"){
         //         //         return false;
         //         // }
         //         // alert("wiw");
-        // }
+        // };
 };
 
 // document.getElementById('form-button').onclick = function () {
