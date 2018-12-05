2003e92e23c0a14b35377853e423577ec771da77
jonavano <jonathanvanoudheusden@gmail.com>
2018-12-05 21:57:34 +0100

    cleaned out code and added all the starting ships at the right location

--- v2/javascripts/app.js
+++ v2/javascripts/app.js
@@ -1,7 +1,10 @@
-var smolship = [55, 55];
+var smolship = [5, 6];
+var smolship2 = [2, 3];
+var mediumship = [16 , 23, 30];
+var mediumship2 = [20, 27, 34];
 
-var largeship = [ 1, 23, 45 , 65];
-var ships = [smolship, largeship];
+var largeship = [ 18, 25, 32 , 39];
+var ships = [smolship, smolship2, mediumship, mediumship2, largeship];
 
 var clicks = 0;
 var hits = 0;
@@ -10,29 +13,11 @@ var hits = 0;
 var main = function () {
     "use strict";
 
-    
-
-    // $(".leftField button").on("click", function(event) {
-        
-    //     console.log(event);
-    //     this.disabled = true;
-
-    // });
-
-    // $(".leftField button:nth-child(5)").on("click", function(event) {
-        
-    //     console.log("wow");
-    //     this.disabled = true;
-    //    $(".leftField button:nth-child(5)").addClass("active");
-
-    // });
-
-    
 
     $(".leftField div").toArray().forEach(function (element) {
         // create a click handler for this element
         $(element).on("click", function () {
-            //$(".tabs a span").removeClass("active");
+           
             var $element = $(element);
             console.log($element);
             $element.addClass("clicked");
@@ -46,18 +31,7 @@ var main = function () {
                 }
         }
 
-            console.log(id);
-            // for(var index = 1; index <= 49; index++){
-            //     if(id == index){
-                    $(".leftField div:nth-child(" + id + ")") 
-                            this.disabled = true;
-                    
-              //  }
-        //    }
-
-
-           // this.disable = true;
-            // element.addClass("active");
+            
 
             clicks ++;
 
@@ -69,34 +43,7 @@ var main = function () {
             }
             console.log(clicks);
 
-        //     var count = 0;
-        //     for(var int = 1; int <= 49; int ++){
-        //         var test = $(".leftField div:nth-child(" +int+")");
-        //         console.log("problem" +  test.hasClass("clicked") + " " + int);
-
-        //         if(test.hasClass("clicked") != true){
-        //                 // test.includes("clicked");
-        //                 count ++;
-                       
-        //                 console.log("this si the problem" +  test.hasClass("clicked") );
-        //                 return false;
-        //         }
-        //         if(count == 49){
-        //                 alert("you lost");
-        //         }
-        //         console.log("it works" + count);
-                
-        //         // if($(".leftField div:nth-child(" +int+")")){
-                        
-        //         //         console.log("this workds");
-        //         // }
-        //         // console.log($(".leftField div:nth-child(" + int +")"));
-        //         // if(document.leftField.getElementById(int).clicked != "clicked"){
-        //         //         return false;
-        //         // }
-        //         // alert("wiw");
-        // };
-        // alert("you lost");
+      
             return false;
         });
         });
@@ -111,47 +58,12 @@ $(".rightField div").toArray().forEach(function (element) {
             $element.addClass("active");
             var id = element.id;
 
-            console.log(id);
-            // for(var index = 1; index <= 49; index++){
-            //     if(id == index){
-                    $(".leftField div:nth-child(" + id + ")") 
-                            this.disabled = true;
-                    
-              //  }
-        //    }
-
             this.disable = true;
-            // element.addClass("active");
-           alert("jdkd");
             return false;
         });
         });
 
-
-        // for(var int = 1; int <= 49; int ++){
-        //         var test = $(".leftField div:nth-child(" +int+")");
-
-        //         if(test.hasClass("clicked") != true){
-        //                 // return false;
-
-        //                 console.log("this si the problem");
-        //         }
-        //         console.log(test.hasClass("clicked"));
-                
-        //         // if($(".leftField div:nth-child(" +int+")")){
-                        
-        //         //         console.log("this workds");
-        //         // }
-        //         // console.log($(".leftField div:nth-child(" + int +")"));
-        //         // if(document.leftField.getElementById(int).clicked != "clicked"){
-        //         //         return false;
-        //         // }
-        //         // alert("wiw");
-        // };
 };
 
-// document.getElementById('form-button').onclick = function () {
-//     this.disabled = true;
-// }
 
 $(document).ready(main);
