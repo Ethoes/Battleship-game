7baffbecaa7e9d058b1ee8462933d2c2a81b339f
jonavano <jonathanvanoudheusden@gmail.com>
2018-12-08 13:08:02 +0100

    added picking the ships location

--- v2/setup.js
+++ v2/setup.js
@@ -6,66 +6,133 @@ var mediumship2 = [20, 27, 34];
 var largeship = [ 18, 25, 32 , 39];
 var ships = [smolship, smolship2, mediumship, mediumship2, largeship];
 
+var $element = 0;
+var toggle = 0;
+var turn = 0;
+
+
 
 var main = function () {
     "use strict";
 
 
-    // $(".Field div").toArray().forEach(function (element) {
-    //     // create a click handler for this element
-    //     $(element).on("click", function () {
+    $(".shipsetc img").toArray().forEach(function (element) {
+
+        $(element).on("click", function () {
+
+
+            if(toggle == 0){
+
+            toggle = 1;
+
+            
+
+            $element = Number(element.id);
+            console.log($element);
+            $(element).addClass("clicked");
+            
+            if($element == 1){
+                turn = 2;
+                smolship = [];
+            };
+
+            if($element == 2){
+                turn = 2;
+                smolship2 = [];
+            };
+
+            if($element == 3){
+                turn = 3;
+                mediumship = [];
+            };
+
+            if($element == 4){
+                turn = 3;
+                mediumship2 = [];
+            };
+
+            if($element == 5){
+                turn = 4;
+                largeship = [];
+            };
+
+
+
+
+        };
+        });
+        
+     });
+    
+
+
+
+
+// if(toggle == 0){
+
+    
+    // if($element > 0){
+        toggle = 0;
+
+    $(".Field div").toArray().forEach(function (elementgrid) {
+
+
+        $(elementgrid).on("click", function () {
+
+            if(toggle == 1){
+
+                console.log("turn =" + turn);
+
+                var idgrid = elementgrid.id;
+
+                if(turn > 0){
+
+                    if($element == 1){
+                     smolship.push(idgrid);
+                     turn--;
+                    }
+
+                    if($element == 2){
+                        smolship2.push(idgrid);
+                        turn--;
+                       }
+
+                       if($element == 3){
+                        mediumship.push(idgrid);
+                        turn--;
+                       }
+
+                       if($element == 4){
+                        mediumship2.push(idgrid);
+                        turn--;
+                       }
+
+                       if($element == 5){
+                        largeship.push(idgrid);
+                        turn--;
+                       }
+
+            }
+
+            if(turn == 0){
+                toggle = 0;
+                $element = null;
+            }
+            
            
-    //         var $element = $(element);
-    //         console.log($element);
-    //         $element.addClass("clicked");
-    //         var id = Number(element.id);
+        }
+        return false;
+    })
 
-    //         for(var index = 0; index < ships.length; index++){
-    //             if(ships[index].includes(id)){
-    //                     console.log("that's a hit");
-    //                     $element.addClass("hit");
-                        
-    //             }
-    //     }
+
+    // }
+    
+})
 
             
-    //     // function allowDrop(ev) {
-    //     //     ev.preventDefault();
-    //     //   }
-          
-    //     //   function drag(ev) {
-    //     //     ev.dataTransfer.setData("text", ev.target.id);
-    //     //   }
-          
-    //     //   function drop(ev) {
-    //     //     ev.preventDefault();
-    //     //     var data = ev.dataTransfer.getData("text");
-    //     //     ev.target.appendChild(document.getElementById(data));
-    //     //   }
-            
 
-      
-    //         return false;
-    //     });
-    //     });
-
-
+        
 };
-
-function allowDrop(ev) {
-    ev.preventDefault();
-  }
-  
-  function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-  }
-  
-  function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-  }
-
 
 
 $(document).ready(main);
