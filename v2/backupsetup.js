636efde531b0a8fe9c38db9f7d11ed13de067170
jonavano <jonathanvanoudheusden@gmail.com>
2018-12-09 09:57:50 +0100

    failure

--- v2/backupsetup.js
+++ v2/backupsetup.js
@@ -107,14 +107,11 @@ var main = function () {
                 console.log("prevgrid before assert " + prevgrid);
 
             console.log(" vertical  " + isshipvertical);
-                console.log( "plus 1 " + ( (isshipvertical == false ) && ( (digrid.includes(prevgrid + 1)) || (digrid.includes(prevgrid - 1)) ) ) );
-                console.log(" not at the edge  " +   ( (idgrid != 7) && (idgrid != 14) && (idgrid != 21) && (idgrid != 28) && (idgrid != 35) && (idgrid != 42) && (idgrid != 49) &&
-                (idgrid != 1) && (idgrid != 8) && (idgrid != 15) && (idgrid != 22) && (idgrid != 29) && (idgrid != 36) && (idgrid != 43)));
-
+                console.log( "plus 1 " + ( (isshipvertical == false ) && ( (digrid.includes(prevgrid + 1)) || (digrid.includes(prevgrid - 1)) ) ) )
                 console.log("plus 7 " +  ( ((isshipvertical == true) || (howmanyturns < 2) )&&    ( (digrid.includes( (prevgrid + 7)  )) || (digrid.includes( (prevgrid - 7) ) ) )));
                 console.log("digrid contains prevgrid " + ( idgrid == prevgrid ) );
 
-                if(( ((isshipvertical == false ) || (howmanyturns < 2)) && ( (digrid.includes(prevgrid + 1)) || (digrid.includes(prevgrid - 1)) || ( idgrid == prevgrid )) )  ){
+                if((prevgrid == (idgrid + 1)) || (prevgrid == (idgrid - 1)) || (prevgrid == (idgrid + 7)) || (prevgrid == (idgrid - 7)) || (prevgrid == (idgrid))  ){
                     
 
 
@@ -123,7 +120,8 @@ var main = function () {
                   
                 if(turn > 0){
 
-        
+                    
+
                     if($element == 1){
                      smolship.push(idgrid);
                      turn--;
@@ -141,7 +139,9 @@ var main = function () {
 
                         if( (digrid.includes(prevgrid + 7)) || (digrid.includes(prevgrid - 7))){
                             isshipvertical = true;
-                            console.log("sup m " + isshipvertical);   
+                            
+                            console.log("sup m " + isshipvertical);
+                            
                         }
                        }
 
@@ -149,6 +149,7 @@ var main = function () {
                         mediumship2.push(idgrid);
                         turn--;
                         howmanyturns++;
+
                         if((digrid.includes(prevgrid + 7)) || (digrid.includes(prevgrid - 7))){
                             isshipvertical = true;
                         }
@@ -158,6 +159,7 @@ var main = function () {
                         largeship.push(idgrid);
                         turn--;
                         howmanyturns++;
+
                         if((digrid.includes(prevgrid + 7)) || (digrid.includes(prevgrid - 7))){
                             isshipvertical = true;
                         }
@@ -165,7 +167,7 @@ var main = function () {
 
             }
 
-             prevgrid = idgrid;
+            prevgrid = idgrid;
 
             if(turn == 0){
                 shipimg.addClass("disable");
@@ -182,85 +184,7 @@ var main = function () {
         
             console.log("test array grid " + digrid + " index " + digrid.includes(prevgrid) + prevgrid ) //(prevgrid == (digrid + 1)) || (prevgrid == (digrid - 1)) || (prevgrid == (digrid + 7)) || (prevgrid == (digrid - 7)) || (prevgrid == (digrid)));
 
-        } 
-
-
-
-
-        if(( ((isshipvertical == true) || (howmanyturns < 2)) &&    ( (digrid.includes( (prevgrid + 7)  )) || (digrid.includes( (prevgrid - 7) ) ) ))  ){
-                    
-
-
-            $(elementgrid).addClass("clicked");
-
-          
-        if(turn > 0){
-
-
-            if($element == 1){
-             smolship.push(idgrid);
-             turn--;
-            }
-
-            if($element == 2){
-                smolship2.push(idgrid);
-                turn--;
-               }
-
-               if($element == 3){
-                mediumship.push(idgrid);
-                turn--;
-                howmanyturns ++;
-
-                if( (digrid.includes(prevgrid + 7)) || (digrid.includes(prevgrid - 7))){
-                    isshipvertical = true;
-                    console.log("sup m " + isshipvertical);   
-                }
-               }
-
-               if($element == 4){
-                mediumship2.push(idgrid);
-                turn--;
-                howmanyturns++;
-                if((digrid.includes(prevgrid + 7)) || (digrid.includes(prevgrid - 7))){
-                    isshipvertical = true;
-                }
-               }
-
-               if($element == 5){
-                largeship.push(idgrid);
-                turn--;
-                howmanyturns++;
-                if((digrid.includes(prevgrid + 7)) || (digrid.includes(prevgrid - 7))){
-                    isshipvertical = true;
-                }
-               }
-
-    }
-
-     prevgrid = idgrid;
-
-    if(turn == 0){
-        shipimg.addClass("disable");
-        toggle = 0;
-        $element = null;
-        prevgrid = null;
-
-        
-        isshipvertical = false;
-        digrid = [];
-        howmanyturns = 0;
-    }
-    
-
-   // console.log("test array grid " + digrid + " index " + digrid.includes(prevgrid) + prevgrid ) //(prevgrid == (digrid + 1)) || (prevgrid == (digrid - 1)) || (prevgrid == (digrid + 7)) || (prevgrid == (digrid - 7)) || (prevgrid == (digrid)));
-
-} 
-
-
-
-
-
+        } // id statements
         }
         return false;
     })
