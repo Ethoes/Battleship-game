959f6109319702769723187bdd118535b8bc9794
jonavano <jonathanvanoudheusden@gmail.com>
2018-12-10 19:20:46 +0100

    added functionality to servesr

--- BATTLESHIP/app.js
+++ BATTLESHIP/app.js
@@ -10,6 +10,90 @@ app.use(express.static(__dirname + "/public"));
 var server = http.createServer(app);
 const wss = new websocket.Server({ server });
 
+var websockets = {};
+
+var currentGame = new Game(gameStats.gamesInitialized++);
+var connectionID = 0;
+
+
+wss.on("connection", function connection(ws) {
+
+  let con = ws; 
+    con.id = connectionID++;
+    let playerType = currentGame.addPlayer(con);
+    websockets[con.id] = currentGame;
+
+    console.log("Player %s placed in game %s as %s", con.id, currentGame.id, playerType);
+
+    if (currentGame.hasTwoConnectedPlayers()) {
+      currentGame = new Game(gameStatus.gamesInitialized++);
+  }
+
+  con.on("message", function incoming(message) {
+  
+    let oMsg = JSON.parse(message);
+ 
+        let gameObj = websockets[con.id];
+        let isPlayerA = (gameObj.playerA == con) ? true : false;
+
+        if (isPlayerA) {
+            
+            /*
+             * player A cannot do a lot, just send the target word;
+             * if player B is already available, send message to B
+             */
+            if (oMsg.type == messages.T_TARGET_WORD) {/////////////////////change message
+                gameObj.setplayerAfield(oMsg.data);
+
+                if(gameObj.hasTwoConnectedPlayers()){
+                    gameObj.playerB.send(message); 
+                }     
+                
+                if(oMsg.type == messages.T_MAKE_A_GUESS){
+                  gameObj.playerA.send(message);
+                  gameObj.setStatus("CHAR GUESSED");
+              }
+
+              if( oMsg.type == messages.T_GAME_WON_BY){
+                gameObj.setStatus(oMsg.data);
+                //game was won by somebody, update statistics
+                gameStatus.gamesCompleted++;
+            }  
+
+            }
+        }
+        else {
+
+          if (oMsg.type == messages.T_TARGET_WORD) { ////////////////////message
+            gameObj.setplayerBfield(oMsg.data);
+
+            if(gameObj.hasTwoConnectedPlayers()){
+                gameObj.playerA.send(message); 
+            }                
+        }
+            /*
+             * player B can make a guess; 
+             * this guess is forwarded to A
+             */ 
+            if(oMsg.type == messages.T_MAKE_A_GUESS){
+                gameObj.playerA.send(message);
+                gameObj.setStatus("CHAR GUESSED");
+            }
+
+            /*
+             * player B can state who won/lost
+             */ 
+            if( oMsg.type == messages.T_GAME_WON_BY){
+                gameObj.setStatus(oMsg.data);
+                //game was won by somebody, update statistics
+                gameStatus.gamesCompleted++;
+            }            
+        }
+    });
+  
+})
+
+
 
 
 app.get("/", function (req, res) {
@@ -34,7 +118,7 @@ app.get("/", function (req, res) {
     since : Date.now(),     /* since we keep it simple and in-memory, keep track of when this object was created */
     shotsfired : 0,   /* number of games initialized */
     playersonline : 0,       /* number of games aborted */
-    gamesCompleted : 0      /* number of games successfully completed */
+    gamesInitialized : 0      /* number of games successfully completed */
 };
 
 
